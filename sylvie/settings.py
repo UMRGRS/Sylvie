205a28159714a734f22476613ff84fcad28bb5ed
UMRGRS <qxrvynilp@gmail.com>
2024-11-08 17:58:12 -0600

    Updated requirements.txt and solved cors problem

--- sylvie/settings.py
+++ sylvie/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = []
 ASGI_APPLICATION = 'sylvie.asgi.application'
 
 INSTALLED_APPS = [
+    'corsheaders',
     'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,6 +62,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
 ]
 
 CHANNEL_LAYERS = {
