17af43e926f488777156a9f47627ef2144f22eed
UMRGRS <qxrvynilp@gmail.com>
2024-11-09 10:44:32 -0600

    Updated requirements.txt

--- sylvie/settings.py
+++ sylvie/settings.py
@@ -62,6 +62,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
+
 ROOT_URLCONF = 'sylvie.urls'
 
 TEMPLATES = [
